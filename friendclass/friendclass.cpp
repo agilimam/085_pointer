2f6a1681135105e96c06d0b5396383222d3f2d6a
agilimam <agilimamalhafizi04@gmail.com>
2023-05-10 11:31:15 +0700

    memanggil kembali program utama

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -10,3 +10,35 @@ public:
 	void setNama(string pNama);
 	friend class siswa;
 };
+
+class siswa {
+private:
+	int id;
+public:
+	void setID(int pID);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+
+}
+
+void siswa::setID(int pID) {
+	id = pID;
+}
+
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+int main()
+{
+	orang o;
+	o.setNama("koko kumat");
+	siswa s;
+	s.setID(1);
+	s.displayAll(o);
+	system("pause");
+	return 0;
+}
